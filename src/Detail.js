bb8938d3512a72dbe5a26e65acc5481ded4b95e4
내꺼다\ysk30 <ysk30430@naver.com>
2022-08-02 13:35:17 +0900

    3-12. 버킷리스트 데이터 삭제하기 기능 추가

--- src/Detail.js
+++ src/Detail.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { useSelector , useDispatch } from "react-redux"
+import { deleteBucket } from "./redux/modules/bucket"; //디스패치 하기위해 임포트
+import { useHistory } from "react-router-dom" //뒤로가기를 사용하기 위해 임포트
 
 
 const Detail = (props) => {
     const params = useParams();
-    console.log(index);
+    // console.log(index);
+    const history = useHistory(); //임포트 후 세트선언
+    const dispatch = useDispatch();
     const bucket_index = params.index;
     const bucket_list = useSelector((state) => state.bucket.list);
+    // console.log(bucket_list);// 뉴버킷리스트 바로 리턴했을때 오류가 난다면확인해바야함
 
-    console.log(bucket_list);
-    console.log(bucket_list[bucket_index])
+    // console.log(bucket_list);
+    // console.log(bucket_list[bucket_index]);
 
 
     return (
-        <h1> {bucket_list[bucket_index]} </h1>
+        <div>
+            <h1> {bucket_list[bucket_index]} </h1>
+            <button onClick={()=> {
+                console.log("삭제해줘!");
+                dispatch(deleteBucket(bucket_index)); //디스패치 하려면 액션생성함수를 임포트 해오ㅏ야함
+                //임포트해온 deleteBucket해오고 안에는 버킷인덱스
+                history.goBack(); //삭제시엔 페이지에 남는 게 없으니 자동 뒤로가기
+            }}>삭제하기</button>
+            </div>
     )
 }
 
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -2,6 +2,7 @@
 
 // Actions type을 정해주는곳
 const CREATE = 'bucket/CREATE';
+const DELETE = "bucket/DELELTE";
 
 //initial state 초기값
 const initialState = {
@@ -14,6 +15,11 @@ export function createBucket(bucket) {
     return {type: CREATE, bucket };
 }
 
+export function deleteBucket(bucket_index) {
+  console.log("지울 버킷 인덱스" , bucket_index);
+  return {type: DELETE, bucket_index};
+}
+
 
 // Reducer
 export default function reducer(state = initialState, action = {}) { 
@@ -24,6 +30,21 @@ export default function reducer(state = initialState, action = {}) {
             const new_bucket_list = [...state.list, action.bucket];
             return {list: new_bucket_list};
             //return a'가 (바꿔서 보여줄값)들어가야하는데/원래있던 배열에 +되는것/위에 선언해줌
+        }
+        case "bucket/DELETE": {
+          console.log(state, action); // 삭제하기 확인 어떤 리스트를 ㅏㄱ지고잇나, 액션개체확인
+          const new_bucket_list = state.list.filter((l, idx)=> {
+            // console.log(
+            //   parseInt(action.bucket_index) !== idx , 
+            //   parseInt(action.bucket_index), 
+            //   idx);
+            return parseInt(action.bucket_index) !== idx; //새 배열은 필터로 거른, 삭제된거만 빼고 나와야하니까
+            //!== 하면 true만 나오니까 != 해주면 되는데 그럼 노란밑줄쳐짐. 그렇다면?? parseInt!
+            console.log(new_bucket_list);
+          })
+          return {list:new_bucket_list};
+          //return new_bucket_list; //이렇게 바로 뉴버킷리스트하면 오류가난다
+          //return 은 state를 뱉어주ㅓ야함 {키, 밸류} 리스트 형식을 맞춰줘야한다
         }
       // do reducer stuff
       default: return state;
